e2b8bd374572d50cf7bbb146a77a0d19943aa75a
Festus Kapkea <kapkeafestus@gmail.com>
2022-09-27 14:29:26 +0300

    Add files via upload

---
+++ Transport_Analysis.py
@@ -0,0 +1,24 @@
+import csv
+import pandas as pd
+import streamlit as st
+
+
+st.write ("""
+
+# Transport Safety App
+
+This web app demonstrates Safety of Transport dataset collected to document security pain points and strategies adopted by different cities in the world to ensure inclusive development in public transit systems""")
+
+
+
+
+csv_file= "G:/Transport Safety Project/Clean Data.csv"
+
+
+data= pd.read_csv (csv_file)
+
+
+
+Gender_Select = st.multiselect('gender:',
+                                    gender,
+                                    default=gender)
